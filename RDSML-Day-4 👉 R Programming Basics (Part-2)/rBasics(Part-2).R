db75af6f81af116b81530daf4cb3d4fed9b677f4
tanbir2104 <gausultanbir88@gmail.com>
2025-05-07 22:51:27 +0600

    Day-4 Practice
    This is the code of mathematical and logical operation

---
+++ RDSML-Day-4 👉 R Programming Basics (Part-2)/rBasics(Part-2).R
@@ -0,0 +1,38 @@
+# Data types in R
+# 1.Numeric - Integer / Floating point
+# 2.String - "Tanbir"
+# 3. Logical
+a = 11
+b = 7.19
+class(a)
+class(b)
+name = "tanbir"
+class(name)
+
+# Logical Data Type - TRUE, FALSE (T,F)
+a > b
+class(a > b)
+x = 19
+y = 22-3
+print(x==y)
+print(x != y)
+print(y >= x)
+
+
+# Logical Operators in R
+4 == 4 && 5 == 5
+# && Logical operator
+# || Logical operator
+x != y || x == y
+# Simple Mathematical Operators in R
+z = 6 + 9 - 8 * (2 / 3)
+print(z)
+w = (4 + 3 - 3) * (2 / 3)
+print(w)
+y = 4^2
+print(y)
+
+
+11 / 5
+11 %% 5
+11 %/% 5
