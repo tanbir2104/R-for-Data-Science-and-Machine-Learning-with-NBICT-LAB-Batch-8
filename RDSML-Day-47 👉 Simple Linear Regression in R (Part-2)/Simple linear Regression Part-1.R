2367af15dfadb7d9f65e59c47ea0959ea7d488b3
tanbir2104 <gausultanbir88@gmail.com>
2025-10-13 22:49:45 +0600

    Simple Linear Regression Part-2

--- RDSML-Day-47 👉 Simple Linear Regression in R (Part-2)/Simple linear Regression Part-1.R
+++ RDSML-Day-47 👉 Simple Linear Regression in R (Part-2)/Simple linear Regression Part-1.R
@@ -2,7 +2,7 @@
 data = read.csv("Salary_Data.csv")
 
 # Splitting the data set into the training set and test set
-install.packages("caTools")
+#install.packages("caTools")
 library(caTools)
 set.seed(123)
 split = sample.split(data$Salary, SplitRatio = 2/3)
@@ -14,4 +14,31 @@ test_set = subset(data, split==FALSE)
 
 # Fitting the simple linear regression to the training set
 regressor = lm(formula = Salary~YearsExperience, data=training_set)
-regressor
+# Checking the summary of the model
+summary(regressor)
+
+# Predicting the test set result
+y_pred = predict(regressor, newdata = training_set)
+y_pred
+
+# Visualizing the training set
+library(ggplot)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience,y =training_set$Salary),
+             colour = "red")+
+  geom_line(aes(x = training_set$YearsExperience,y=y_pred,
+                 colour = "blue"))+
+  ggtitle("salary vs Experience")+
+  xlab("years of experience")+
+  ylab("salary")
+
+# Visualizing the test set result
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience,y =test_set$Salary),
+             colour = "red")+
+  geom_line(aes(x = test_set$YearsExperience,y=predict(regressor, newdata = test_set),
+                colour = "blue"))+
+  ggtitle("salary vs Experience")+
+  xlab("years of experience")+
+  ylab("salary")
+
